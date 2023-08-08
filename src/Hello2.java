0cc6da585ab8521ec3df73d91820273fd0466a21
AseFurkan <furkan1asefler@gmail.com>
2023-08-08 15:35:20 +0300

    Task 1

---
+++ src/Hello2.java
@@ -0,0 +1,9 @@
+public class Hello2 {
+    public static void main(String[] args) {
+        // Comment line
+        System.out.print("Hello "); // we will see Hello in screen
+        // Attention I deleted "ly"
+        System.out.print("World"); // we will see Hello World in screen
+
+    }
+}
---
+++ src/Hello3.java
@@ -0,0 +1,22 @@
+public class Hello3 {
+    public static void main(String[] args) {
+        // if you meet a bug,firstly  review  parenthesis
+        System.out.println("Hello");
+        System.out.println("World");
+        // screen =
+        // Hello
+        // World
+        System.out.print("Hello");
+        System.out.print("World");
+        // screen =
+        // HelloWorld
+        System.out.print("Hello ");
+        System.out.print("World");
+        // screen =
+        // Hello World
+        System.out.print("*Hello-");
+        System.out.print("-World*");
+        // screen =
+        // *Hello--World*
+    }
+}
